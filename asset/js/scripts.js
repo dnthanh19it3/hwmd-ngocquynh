5c6f209a903cadbb1389b80d99e905993b1ac916
ThanhDN <thanhstp99@gmail.com>
2025-10-19 23:49:33 +0700

    Update content

---
+++ asset/js/scripts.js
@@ -0,0 +1,23 @@
+function load2(){
+    $("#block").load("asset/txt/content2.txt");
+    var myAudio = new Audio('asset/audio/the_way.m4a');
+    myAudio.play();
+}
+function load3(){
+    $("#block").load("asset/txt/content3.txt");
+}
+function load4(){
+    $("#block").load("asset/txt/content4.txt");
+}
+function load5(){
+    $("#block").load("asset/txt/content5.txt");
+}
+function load6(){
+    $("#block").load("asset/txt/content6.txt");
+}
+function load7(){
+    $("#block").load("asset/txt/content7.txt");
+}
+function load8(){
+    $("#block").load("asset/txt/content-credit.txt");
+}
